ae2bab8f119cd252c67b23147506b624873b0a80
gsc <gabrielschincariol@gmail.com>
2021-08-16 22:55:42 -0300

    Fixed capitalization

--- sources/mediumApp.js
+++ sources/mediumApp.js
@@ -6,15 +6,20 @@ const spinner = document.querySelector('.spinner');
 const parser = new RSSParser();
 const CORS_PROXY = "https://api.codetabs.com/v1/proxy?quest="
 
+const capitalize = (string) => {
+    if (typeof string !== 'string') return ''
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
 parser.parseURL(CORS_PROXY + 'https://medium.com/feed/@gschincariol', function (err, feed) {
     if (err) throw err;
     console.log(feed.title);
-    lastTextTitle.textContent = feed.items[0].title;
+    lastTextTitle.textContent = capitalize(feed.items[0].title.toLowerCase());
     lastTextURL.href = feed.items[0].link;
     for (let i = 1; i <= 9; i++) {
         let newText = document.createElement('div');
         newText.className = "lastTextItem";
-        newText.innerHTML = `<a class="lastTextURL" href="${feed.items[i].link}" target="_blank" rel="noopener noreferrer"><p class="textTitle">${feed.items[i].title}</p></a>`;
+        newText.innerHTML = `<a class="lastTextURL" href="${feed.items[i].link}" target="_blank" rel="noopener noreferrer"><p class="textTitle">${capitalize(feed.items[i].title.toLowerCase())}</p></a>`;
         lastTextList.appendChild(newText)
     }
     spinner.classList.toggle('d-none');
